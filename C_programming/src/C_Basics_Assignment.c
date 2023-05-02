aa2163dddaee707206fb0513cc238ed80edf9414
Mostafa Shams <mosstafashamss@gmail.com>
2023-05-02 18:43:44 +0300

    altered main function

--- C_programming/src/C_Basics_Assignment.c
+++ C_programming/src/C_Basics_Assignment.c
@@ -8,7 +8,7 @@
 void ex1()
 {
 	printf("***EX1_Homework1***\n");
-	printf(" C programming\n");
+	printf(" C programming\n\n");
 }
 
 
@@ -21,14 +21,13 @@ void ex2()
 	flush
 	scanf("%d",&num);
 
-	printf("You entered: %d\n",num);
+	printf("You entered: %d\n\n",num);
 }
 
 
 //EX3:
 int ex3(int num1, int num2)
 {
-	printf("***EX3_Homework1***\n");
 	int sum = num1 + num2;
 	return sum;
 }
@@ -42,7 +41,7 @@ void ex5()
 	printf("Enter a character: ");
 	flush
 	scanf("%c", &c);
-	printf("ASCII value of %c = %d\n",c,(int)c);
+	printf("ASCII value of %c = %d\n\n",c,(int)c);
 }
 
 
@@ -62,8 +61,8 @@ void ex7()
 	b=a-b;
 	a=a-b;
 
-	printf("\nNew value of a is: %f\n", a);
-	printf("New value of b is: %f\n", b);
+	printf("\nNew value of a is: %f\n\n", a);
+	printf("New value of b is: %f\n\n", b);
 
 }
 
@@ -80,9 +79,9 @@ void ex_1()
 	scanf("%d", &num);
 
 	if(num%2==0)
-		printf("%d is even\n", num);
+		printf("%d is even\n\n", num);
 	else
-		printf("%d is odd\n", num);
+		printf("%d is odd\n\n", num);
 }
 
 
@@ -105,10 +104,10 @@ void ex_2()
 	case 'E':
 	case 'I':
 	case 'O':
-		printf("%c is a vowel.\n", c);
+		printf("%c is a vowel.\n\n", c);
 		break;
 	default:
-		printf("%c is a consonant.\n", c);
+		printf("%c is a consonant.\n\n", c);
 		break;
 	}
 }
@@ -128,7 +127,7 @@ void ex_3()
 		max=num2;
 	if(num3>max)
 		max=num3;
-	printf("Largest number: %f\n",max);
+	printf("Largest number: %f\n\n",max);
 }
 
 
@@ -139,12 +138,12 @@ void ex_5()
 	printf("***EX5_Homework2***\n");
 	printf("Enter a character: ");
 	flush
-	scanf("%c", c);
+	scanf("%c", &c);
 
 	if((c>'a' && c<'z') || (c>'A' && c<'Z'))
-		printf("%c is an alphabet\n", c);
+		printf("%c is an alphabet\n\n", c);
 	else
-		printf("%c is not an alphabet\n",c);
+		printf("%c is not an alphabet\n\n",c);
 }
 
 
@@ -163,12 +162,12 @@ void ex_7()
 		{
 			fact*=i;
 		}
-		printf("factorial = %d\n", fact);
+		printf("factorial = %d\n\n", fact);
 	}
 	else if(num==0)
-		printf("factorial = 1\n");
+		printf("factorial = 1\n\n");
 	else
-		printf("error!!! factorial of a negative number does not exist\n");
+		printf("error!!! factorial of a negative number does not exist\n\n");
 }
 
 
@@ -189,22 +188,22 @@ void ex_8()
 	switch(c)
 	{
 	case '+':
-		printf("%f + %f = %f\n",num1, num2, (num1+num2));
+		printf("%f + %f = %f\n\n",num1, num2, (num1+num2));
 		break;
 	case '-':
-			printf("%f - %f = %f\n",num1, num2, (num1-num2));
+			printf("%f - %f = %f\n\n",num1, num2, (num1-num2));
 			break;
 	case '*':
-			printf("%f * %f = %f\n",num1, num2, (num1*num2));
+			printf("%f * %f = %f\n\n",num1, num2, (num1*num2));
 			break;
 	case '/':
 			if(num2!=0)
-				printf("%f / %f = %f\n",num1, num2, (num1/num2));
+				printf("%f / %f = %f\n\n",num1, num2, (num1/num2));
 			else
-				printf("error! operand 2 = 0\n");
+				printf("error! operand 2 = 0\n\n");
 			break;
 	default:
-			printf("please enter one of the mentioned operators\n");
+			printf("please enter one of the mentioned operators\n\n");
 			break;
 	}
 
@@ -216,21 +215,35 @@ void ex_8()
 int main(void)
 {
 
-//	ex1();
+//	Homework 1:
+	ex1();
 
-//	ex2();
+	ex2();
 
 //	ex3:
-//	int num1, num2;
-//	printf("Enter two integers: ");
-//	fflush(stdin); fflush(stdout);
-//	scanf("%d",&num1);
-//	scanf("%d",&num2);
-//	printf("%d\n", ex3(num1,num2));
+	printf("***EX3_Homework1***\n");
+	int num1, num2;
+	printf("Enter two integers: ");
+	flush
+	scanf("%d",&num1);
+	scanf("%d",&num2);
+	printf("%d\n\n", ex3(num1,num2));
 
-//	ex5();
+	ex5();
 
-//	ex7();
+	ex7();
+
+//	Homework 2:
+
+	ex_1();
+
+	ex_2();
+
+	ex_3();
+
+	ex_5();
+
+	ex_7();
 
 	ex_8();
 
